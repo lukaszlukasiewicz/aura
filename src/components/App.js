a64f23be39d3747c55e170e4b2c819c942804b0b
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2020-12-06 22:36:21 +0100

    Basic Layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import {useState} from "preact/hooks";
-import Styles from "./App.scss";
+import Styles from "./App.module.scss";
 import ProductView from "components/ProductView/ProductView";
+import Sidebar from "components/UI/Sidebar";
+import Panel from "components/UI/Panel"
 const App = () => {
 
 	const [product, setProduct] = useState({
@@ -20,14 +22,31 @@ const App = () => {
 		insert: false,
 	})
 
-	const updateProductState = updatedProductProps => {
+	const updateProduct = updatedProductProps => {
 		const newProductState = Object.assign({},product,updatedProductProps);
 		setProduct(newProductState);
 		return newProductState;
 	}
 
 	return (<div className={Styles.App} id="app">
-		<ProductView product={product} />
+		<div>
+			<ProductView product={product} />
+		</div>
+		<Sidebar>
+			<Panel title="test panel">
+				<div>
+					Panel content;
+					<button onClick={ () => {
+						updateProduct({
+							material : {
+								type: "composite",
+								color: "silver",
+							}
+						})
+					}}>Composite</button>
+				</div>
+			</Panel>
+		</Sidebar>
 	</div>)
 }
 
