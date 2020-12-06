079df075c0d720b1df6cfa7dc491d200a3171372
lukasz.lukasiewicz <lukasz.lukasiewicz@gmail.com>
2020-12-06 23:19:09 +0100

    Change bundle name

--- preact.config.js
+++ preact.config.js
@@ -1,14 +1,14 @@
   
 export default (config, env, helpers) => {
   config.output.publicPath = "./";
-  config.output.filename = "[name].js";
+  config.output.filename = pathData =>  pathData.chunk.name == 'bundle' ? 'auraConfigurator.js': '[name].js';
 
   config.resolve.modules.push(env.src)
   delete config.output.chunkFilename;
   config.module.rules[4].use[1].options.url = false;
 
   let { plugin } = helpers.getPluginsByName(config, "MiniCssExtractPlugin")[0];
-  plugin.options.filename = '[name].css';
+  plugin.options.filename = 'auraConfigurator.css';
 
   if (env.production) {
     config.output.libraryTarget = "umd";
