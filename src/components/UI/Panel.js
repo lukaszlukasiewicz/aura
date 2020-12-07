096a6652c1c4a1bfec40576eae2acf3f891289b3
lukaszlukasiewicz <lukasz.lukasiewicz@gmail.com>
2020-12-07 11:03:38 +0100

    Add toggle functionality to Panel component

--- src/components/UI/Panel.js
+++ src/components/UI/Panel.js
@@ -1,11 +1,22 @@
+import {useState} from 'preact/hooks';
 import Styles from "./Panel.module.scss";
 const Panel = props => {
+  const [expanded, setExpandState]  = useState(true);
+
+  const togglePanelContent = () => {
+    setExpandState(!expanded)
+  }
   return(
     <div className={`${Styles.Panel} ${props.className}`}>
-      {props.title && <h2 className={Styles.Panel__title}>{props.title}</h2>}
-      <div>
+      {props.title && <div className={Styles.Panel__title} onClick={togglePanelContent}>
+        <h2>{props.title}</h2>
+        <div className={Styles.Panel__toggle} >
+          <span className={expanded && Styles['Panel__toggle--expanded']}></span>
+        </div>
+      </div>}
+      {expanded && <div className={Styles.Panel__content}>
         {props.children}
-      </div>
+      </div>}
     </div>
   )
 }
