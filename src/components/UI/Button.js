511034c41cc496a197a9945a5bada023ad211414
lukaszlukasiewicz <lukasz.lukasiewicz@gmail.com>
2020-12-07 14:12:04 +0100

    Add active prop to Button component

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,7 +1,8 @@
 import Styles from './Button.module.scss';
 const Button = props => {
-  const {label,iconUrl, bigIcon,round, ...buttonProps} = props;
+  const {active, label,iconUrl, bigIcon,round, ...buttonProps} = props;
   buttonProps.className = buttonProps.className ? `${Styles.Button} ${buttonProps.className}`: Styles.Button; 
+  if(active) buttonProps.className += ` ${Styles["Button--active"]}`;
   return (
     <button {...buttonProps}>
       {iconUrl && <span className={`${Styles.Button__icon} ${bigIcon && Styles['Button__icon--big']} ${round && Styles['Button__icon--round']}`} style={{backgroundImage:`url(${iconUrl})`}}></span>}
