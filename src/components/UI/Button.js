756c06709baf0eb11786b0070d1aad4d473c5e1f
lukaszlukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-01-07 20:50:25 +0100

    Add icon color prop

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,11 +1,11 @@
 import Styles from './Button.module.scss';
 const Button = props => {
-  const {active, label,iconUrl, bigIcon,round, ...buttonProps} = props;
+  const {active, label,iconUrl, iconColor, bigIcon,round, ...buttonProps} = props;
   buttonProps.className = buttonProps.className ? `${Styles.Button} ${buttonProps.className}`: Styles.Button; 
   if(active) buttonProps.className += ` ${Styles["Button--active"]}`;
   return (
     <button {...buttonProps}>
-      {iconUrl && <span className={`${Styles.Button__icon} ${bigIcon && Styles['Button__icon--big']} ${round && Styles['Button__icon--round']}`} style={{backgroundImage:`url(${iconUrl})`}} />}
+      {(iconUrl || iconColor ) && <span className={`${Styles.Button__icon} ${bigIcon && Styles['Button__icon--big']} ${round && Styles['Button__icon--round']}`} style={{backgroundImage:`url(${iconUrl})`,backgroundColor:iconColor}} />}
       {label}
     </button>
   )
