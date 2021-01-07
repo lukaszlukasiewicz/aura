d332dbdfd163cb4202356eabc6636df900ac9676
lukaszlukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-01-07 22:47:39 +0100

    Add reducer Rules and cache

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,15 +1,21 @@
 const { createContext } = require("preact");
 import {useReducer} from "preact/hooks";
 
-const initialProductState = {
-  type : "standard",
-  material: "wood",
-  materialColor: {
+const cache = {
+  woodColor:  {
     wood: "okume",
     color:"natural",
   },
+  composieteColor: "RAL 1000",
+  cornerColor: "silver",
+}
+
+const initialProductState = {
+  type : "standard",
+  material: "wood",
+  materialColor:cache.woodColor,
   corners : "round",
-  cornerColor: "wood",
+  cornerColor: cache.cornerColor,
   insert: false,
 };
 
@@ -20,9 +26,19 @@ const getPath = (path,object) => {
 
 const reducer = (product,updatedProps) => {
 
+  // Save values to cache
+  if(product.material == "wood" && updatedProps.materialColor) cache.woodColor = updatedProps.materialColor;
+  if(product.material == "composite" && updatedProps.materialColor) cache.composieteColor = updatedProps.materialColor;
+  if(product.material == "wood" && updatedProps.cornerColor) cache.cornerColor = updatedProps.cornerColor;
   
   if(updatedProps.material == "wood" && product.insert == "lamela" ) updatedProps.insert = false;
   if(updatedProps.material == "composite" && product.insert == "lechuza" ) updatedProps.insert = false;
+
+  if(updatedProps.material == "composite") delete product.cornerColor; 
+  if(updatedProps.material == "wood") product.cornerColor = cache.cornerColor;
+
+  if(updatedProps.material == "wood") product.materialColor = cache.woodColor;
+  if(updatedProps.material == "composite") product.materialColor = cache.composieteColor;
 
   const newState = Object.assign({},product,updatedProps);
   return newState; 
