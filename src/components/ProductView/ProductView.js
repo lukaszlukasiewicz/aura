cc01b4369d230e52ed135aa8f5e733df8fc90dcc
lukaszlukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-01-29 18:00:51 +0100

    Add polyfill for createImageBitmap

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -3,16 +3,31 @@ import {useContext,useState,useEffect} from "preact/hooks"
 import ProductContext from 'contexts/ProductContext';
 import {ral} from "components/UI/RalPalette";
 
+
+// Create  image Boitmap polyfill for Safari
 if (!('createImageBitmap' in window)) {
-  window.createImageBitmap = async function(blob) {
-      return new Promise((resolve,reject) => {
-          let img = document.createElement('img');
-          img.addEventListener('load', function() {
-              resolve(this);
-          });
-          img.src = URL.createObjectURL(blob);
-      });
-  }
+	window.createImageBitmap = async function (data) {
+		return new Promise((resolve,reject) => {
+			let dataURL;
+			if (data instanceof Blob) {
+				dataURL = URL.createObjectURL(data);
+			} else if (data instanceof ImageData) {
+				const canvas = document.createElement('canvas');
+				const ctx = canvas.getContext('2d');
+				canvas.width = data.width;
+				canvas.height = data.height;
+				ctx.putImageData(data,0,0);
+				dataURL = canvas.toDataURL();
+			} else {
+				throw new Error('createImageBitmap does not handle the provided image source type');
+			}
+			const img = document.createElement('img');
+			img.addEventListener('load',function () {
+				resolve(this);
+			});
+			img.src = dataURL;
+		});
+	};
 }
 
 const loadImage = (url) => {
