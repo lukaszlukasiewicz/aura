51c89abacb76ab13470104bc4593d3bd6a8bf83a
lukaszlukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-03-24 20:50:42 +0100

    Add config file

--- src/config.js
+++ src/config.js
@@ -2,56 +2,69 @@ const woodTypes = ["okume","olcha"];
 const woodColors = ["natural","ash-gray","tvt-5060","tvt-5056","5073","5077"]
 
 
-const basic = {
-
+const typeLabels = {
+  stadard : "Standardowa",
+  double : "Podwójna",
+  tall : "Wysoka"
 }
 
 const wood = {
+  material : {
+    value:"wood",
+  },
   type : {
-    expression : /^(standard|wide|tall)$/,
-    value : "standard",
+    values:['standard','double','tall'],
+    default : "standard",
+    labels : typeLabels,
   },
   corners :  {
     expression: /^(round|sharp)$/,
-    value: "round",
+    default: "round",
   },
   insert : {
-    expression: /^lamela$/,
-    value: 'lamela',
+    values : ["Lechuza"],
     empty : true,
+    default: false,
   },
   woodType: {
     expression: new RegExp(`^(${woodTypes.join("|")})$`),
-    value: 'okume',
+    default: 'okume',
   },
   woodColor : {
     expression:  new RegExp(`^(${woodColors.join("|")})$`),
-    value: 'okume',
+    default: 'natural',
   },
 };
 
 const metal = {
   ... wood, // extends wood type
+  material : {
+    value:"metal",
+  }, // overwrite material prop
   cornerColor : {
     expression : /^(silver|graphite|RAL\s(?:\d{4}))$/,
-    value: "silver",
+    default: "silver",
   }
 };
 
 const composite = {
+  material : {
+    value: "composite",
+  },
   type : {
-    expression : /^(standard|wide)$/,
-    value : "standard",
+    values:['standard','double'],
+    default : "standard",
+    labels : typeLabels,
   },
   color: {
-    expresion: /^RAL\s(?:\d{4})$/,
-    value: "RAL 4010"
+    expression: /^RAL\s(?:\d{4})$/,
+    default: "RAL 4010"
   },
   insert : {
-    expression: /^lamela$/,
-    value: 'lamela',
+    values : ["Lamela"],
     empty : true,
+    default: false,
   },
 }
 
-export default [wood,metal,composite];
+export default {wood,metal,composite};
