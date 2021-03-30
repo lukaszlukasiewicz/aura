6982a12de100fcf269e12bf8d1d92ad3bbc09cc8
lukaszlukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-03-30 23:09:23 +0200

    Change product image from canvas to image:data/png

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,5 +1,5 @@
-import {useProductImage} from "hooks/useProductimage";
-import {useRef, useEffect} from "preact/hooks";
+import {useProductImage} from "hooks/useProductImage";
+import {useState} from "preact/hooks";
 import Styles from './CartItem.module.scss';
 
 const imagesAttrs = {
@@ -58,48 +58,49 @@ const getProductDescription = product => {
   return "Opis produktu";
 }
 
-const productPrice = (product,amount) => {
-  const price = 300;
+const productPrice = (price) => {
   return (
     <div className={Styles.ProductPrice}>
-      <h3>{price} pln</h3><span>netto: {(price / 1.23).toFixed(2)} pln</span>
+      <h3>{price.unit.gross.text}</h3><span>{price.unit.net.text}</span>
     </div>
   )
 }
 
+const getImage = async (product) => {
+  const {ratio,x,y} = imagesAttrs[product.type] || {ratio:2,x:100,y:100};
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  const productImages = useProductImage(product);
+  const images = await productImages;
+  canvas.width = images[0].width/ratio;
+  canvas.height = images[0].height/ratio;
+  images.forEach(image => {
+    context.drawImage(image,x,y,image.width,image.height)
+  });
+  return canvas.toDataURL("image/png");
+}
+
 export const CartItem = props => {
-  const canvasRef = useRef();
-  const {id,product,amount} = props.product;
-  useEffect(() =>{
-    const canvas = canvasRef.current;
-    if(!canvas || !product ) return false;
-    const context = canvas.getContext('2d');
-    const {ratio,x,y} = imagesAttrs[product.type] || {ratio:2,x:100,y:100};
-    if(product.image) {
-      canvas.width = product.image.width;
-      canvas.height = product.image.height; 
-      context.putImageData(product.image,0,0)
-    } else {
-      const productImage = useProductImage(product);
-      productImage.then(images=>{
-        canvas.width = images[0].width/ratio;
-        canvas.height = images[0].height/ratio;
-        images.forEach(image => {
-          context.drawImage(image,x,y,image.width,image.height)
-        })
-      }).then( () => {
-        product.image = context.getImageData(0,0,canvas.width,canvas.height);
-      })
-    }
-  },[canvasRef.current,product]);
+
+  const {id,product,price,amount} = props.product;
+  const {updateAmount} = props;
+  //TODO: Move image genaretion to function that is adding product to cart
+  // Not nice way to tell react that new image was generated and it needs to refresh; 
+  const [generatedImage, setGeneratedImag] = useState(false)
+  if(!product.image) {
+    getImage(product).then(image => {
+      product.image = image;
+      setGeneratedImag(true);
+    } );
+  }
   return (<li className={Styles.CartItem} key={id}>
-    <canvas ref={canvasRef} className={Styles.ProductImage} />
+    <img src={product.image}  className={Styles.ProductImage}/>
     <div className={Styles.ProductInfo}>
       <h2>{getProductName(product)}</h2>
       <p>{getProductDescription(product)}</p>
     </div>
-    <div className={Styles.ProductAmount}><input type="number" value={amount} min="1" /></div>
-    <div className={Styles.ProductPrice}>{productPrice(product,amount)}</div>
+    <div className={Styles.ProductAmount}><input type="number" value={amount} min="1" onChange={e => updateAmount(e.target.value)} /></div>
+    <div className={Styles.ProductPrice}>{productPrice(price)}</div>
   </li>)
 }
 
