af8c1c6866bd3708688ac81449692288826fe995
lukaszlukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-04-20 20:57:25 +0200

    Add useProduct Thumb hook

---
+++ src/hooks/useProductThumb.js
@@ -0,0 +1,44 @@
+import {useState} from "preact/hooks"
+import {useProductImage} from "hooks/useProductImage"
+
+const imagesAttrs = {
+  'standard' : {
+    ratio : 3,
+    x : -200,
+    y: -200,
+  },
+  'double' : {
+    ratio : 2,
+    x : -100,
+    y: -150,
+  },
+  'tall' : {
+    ratio : 2,
+    x : -200,
+    y: -100,
+  },
+}
+
+const getImage = async (product) => {
+  const {ratio,x,y} = imagesAttrs[product.type] || {ratio:2,x:100,y:100};
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  const productImages = useProductImage(product);
+  const images = await productImages;
+  canvas.width = images[0].width/ratio;
+  canvas.height = images[0].height/ratio;
+  images.forEach(image => {
+    context.drawImage(image,x,y,image.width,image.height)
+  });
+  return canvas.toDataURL("image/png");
+}
+
+export const useProductThumb = product => {
+  const [image, setImage] = useState(false);
+  if(!image) {
+    getImage(product).then(image => {
+      setImage(image);
+    });
+  }
+  return image
+}
