85f2db09cf612768ba682254d55fd41c19172ffa
lukaszlukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-04-21 22:41:25 +0200

    add Ask Form popup

---
+++ src/components/AskForm/AskForm.js
@@ -0,0 +1,45 @@
+import Styles from './AskForm.module.scss'
+import Close from "assets/icons/close.svg"
+import Mail from "assets/icons/mail.svg"
+import {useSVG} from "hooks/useSVG"
+import {useState, useRef, useLayoutEffect} from "preact/hooks"
+
+const AskForm = props => {
+  const [visible, setVisible] = useState(false);
+  const askRef = useRef();
+  
+  useLayoutEffect(() => {
+    const wrapper = askRef.current;
+    if(!wrapper) return false;
+    const container = wrapper.firstElementChild;
+    wrapper.addEventListener('click', e => {
+      if(!container.contains(e.target)) setVisible(false);
+    })
+  },[visible])
+
+  window.auraAsk = () => {
+    setVisible(true);
+  }
+
+  return  visible && <div className={Styles.AskForm__wrapper} ref={askRef}>
+    <div className={Styles.AskForm__container}>
+      <div className={Styles.AskForm__header}>
+        <h2>Wyślij Zapytanie</h2>
+        <div>
+          <button onClick={e => setVisible(false)}>{useSVG(Close)}</button>
+        </div>
+      </div>
+        <form>
+          <input type="text" placeholder="Imię i nazwisko"  />
+          <input type="text" placeholder="Adrres email"  />
+          <input type="text" placeholder="Telefon"  />
+          <textarea rows="10" placeholder="Twoja wiadomość"></textarea>
+          <div className={Styles.AskForm__submit}>
+            <button>Wyślij {useSVG(Mail)}</button>
+          </div>
+        </form>
+    </div>
+  </div>
+}
+
+export default AskForm;
