1a626e91fb9982ea799f3cda60d6426d2eda0367
lukaszlukasiewicz <lukasz.lukasiewicz@gmail.com>
2021-04-21 18:57:40 +0200

    Add icon and hover state to AddToList button

--- src/components/UI/AddToList.js
+++ src/components/UI/AddToList.js
@@ -1,8 +1,9 @@
 import {useContext} from "preact/hooks";
 import ProductContext from 'contexts/ProductContext';
-import Styles from './AddToCart.module.scss';
+import Styles from './AddToList.module.scss';
 import {useProductThumb} from "hooks/useProductThumb";
 import Star from "assets/icons/star.svg";
+import {useSVG} from "hooks/useSVG";
 
 export default props => {
   const {product} = useContext(ProductContext);
@@ -11,7 +12,7 @@ export default props => {
     <div className={Styles.AddToCart}>
       <button onClick={e => {
         props.add(Object.assign({},product,{image:productThumb}))
-      }} >Dodaj do Listy</button>
+      }} >Dodaj do Listy {useSVG(Star)}</button>
     </div>
   )
 }
